448626ed45c154898f75a32f6f5cd329bb320da2
PenneyGadget <dreamofgreen@gmail.com>
2016-02-29 22:28:19 -0700

    Renders testimonials on landing page

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -10,13 +10,13 @@ RSpec.feature "Landing page", :type => :feature do
     expect(page).to have_css("div.tagline")
   end
 
-  # scenario "User scrolls down landing page" do
-  #   visit root_path
-  #
-  #   expect(page).to have_css("#user-testimonials")
-  #   expect(page).to have_text("How it Works")
-  #   expect(page).to have_css("#how-it-works")
-  #   expect(page).to have_css(".screenshots")
-  #   expect(page).to have_button("Login with Facebook")
-  # end
+  scenario "User scrolls down landing page" do
+    visit root_path
+
+    expect(page).to have_css("div.testimonials")
+    # expect(page).to have_text("How it Works")
+    # expect(page).to have_css("#how-it-works")
+    # expect(page).to have_css(".screenshots")
+    # expect(page).to have_button("Login with Facebook")
+  end
 end
