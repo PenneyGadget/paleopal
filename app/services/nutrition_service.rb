92a5d7797c7ba7c940eba81cc77c8ddb721c0754
PenneyGadget <dreamofgreen@gmail.com>
2016-03-02 17:29:40 -0700

    Begins API implementation

--- app/services/nutrition_service.rb
+++ app/services/nutrition_service.rb
@@ -2,7 +2,23 @@ class NutritionService
   attr_reader :current_user, :connection
 
   def initialize(current_user)
-
+    @current_user = current_user
+    @connection = Faraday.new(:url => "https://api.edamam.com/api/nutrition-details") do |faraday|
+      faraday.adapter Faraday.default_adapter
+      faraday.params["app_id"] = EDAMAM_RA_APP_ID
+      faraday.params["app_key"] = EDAMAM_RA_APP_KEY
+    end
   end
 
 end
+
+# ## POST ##
+#
+# conn.post '/nigiri', { :name => 'Maguro' }  # POST "name=maguro" to http://sushi.com/nigiri
+#
+# # post payload as JSON instead of "www-form-urlencoded" encoding:
+# conn.post do |req|
+#   req.url '/nigiri'
+#   req.headers['Content-Type'] = 'application/json'
+#   req.body = '{ "name": "Unagi" }'
+# end
