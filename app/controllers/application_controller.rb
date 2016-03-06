43d8891295d54c64f7153ea54960803a3d48d0b6
PenneyGadget <dreamofgreen@gmail.com>
2016-03-05 19:19:55 -0700

    Completes all dashboard logic except Ajax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,47 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user,
+                :get_day_entries,
+                :day_breakfast,
+                :day_lunch,
+                :day_dinner,
+                :day_snack,
+                :get_day_totals
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def get_day_entries
+    current_user.entries.select("date").group("date").order(date: :desc)
+  end
+
+  def day_breakfast(day_entry)
+    data = current_user.entries.where(date: day_entry.date, meal: "breakfast").pluck(:fat, :carbs, :protein).flatten
+    {fat: data[0], carbs: data[1], protein: data[2]}
+  end
+
+  def day_lunch(day_entry)
+    data = current_user.entries.where(date: day_entry.date, meal: "lunch").pluck(:fat, :carbs, :protein).flatten
+    {fat: data[0], carbs: data[1], protein: data[2]}
+  end
+
+  def day_dinner(day_entry)
+    data = current_user.entries.where(date: day_entry.date, meal: "dinner").pluck(:fat, :carbs, :protein).flatten
+    {fat: data[0], carbs: data[1], protein: data[2]}
+  end
+
+  def day_snack(day_entry)
+    data = current_user.entries.where(date: day_entry.date, meal: "snack").pluck(:fat, :carbs, :protein).flatten
+    {fat: data[0], carbs: data[1], protein: data[2]}
+  end
+
+  def get_day_totals(day_entry)
+    totals = {}
+    totals[:fat] = current_user.entries.where(date: day_entry.date).pluck("fat").sum
+    totals[:carbs] = current_user.entries.where(date: day_entry.date).pluck("carbs").sum
+    totals[:protein] = current_user.entries.where(date: day_entry.date).pluck("protein").sum
+    totals
   end
 end
